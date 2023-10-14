d3a8ca3c3a31186f37ac1835210f79e02b33db4e
MatthewZGong <matthewzgong@gmail.com>
2023-10-14 16:03:17 -0400

    added skeleton code for user testing api endoints for user reports

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -4,3 +4,17 @@ import server.endpoints as ep
 
 def test_hello():
     assert True
+
+def test_update_job_postings():
+    assert True
+
+def test_delete_account():
+    assert True
+
+def test_get_user_reports():
+    assert True
+
+def test_send_user_report():
+    assert True
+
+
