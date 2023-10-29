53649e624f78151d8f76aa27e3afac3c6493373b
MichelleLin5 <ml7188@nyu.edu>
2023-10-29 19:21:04 -0400

    endpoints

--- server/endpoints.py
+++ server/endpoints.py
@@ -283,7 +283,8 @@ class CreateAccount(Resource):
                        "email/userid": email,
                        "password": password}
         if information != 1:
-            return {"status": "failure", "message": "Fail to create account"}, 200
+            return {"status": "failure", 
+                    "message": "Fail to create account"}, 200
         return {"status": "success",
                 "message": "Account successfully created"}, 200
 
