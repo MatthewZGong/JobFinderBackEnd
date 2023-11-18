7f3b13458604754e9500877291d9117f7b374a6b
TanzHasan <tanzirhasanhasan@gmail.com>
2023-11-18 15:41:11 -0800

    fixed slight error where _id was string instead of ObjectId

--- db/tests/test_db.py
+++ db/tests/test_db.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 import db.db as db
 import pytest
 import db.db_connect as dbc
@@ -6,8 +7,8 @@ TEST_DB = dbc.DB_NAME
 
 @pytest.fixture(scope='function')
 def temp_user():
-    user_id = "507f1f77bcf86cd799439011"
-    job_id = "507f191e810c19729de860ea"
+    user_id = ObjectId("507f1f77bcf86cd799439011")
+    job_id = ObjectId("507f191e810c19729de860ea")
     dbc.client[TEST_DB]["users"].insert_one({"_id": user_id, "username": "GeometryDash"})
     dbc.client[TEST_DB]["jobs"].insert_one({"_id": job_id, "description": "Janitor"})
     # yield to our test function
@@ -16,14 +17,14 @@ def temp_user():
     dbc.client[TEST_DB]["jobs"].delete_one({"_id": job_id})
 
 def test_add_user_report_bad():
-    user_id = "507f1f77bcf86cd799439011"
-    job_id = "507f191e810c19729de860ea"
+    user_id = ObjectId("507f1f77bcf86cd799439011")
+    job_id = ObjectId("507f191e810c19729de860ea")
 
     with pytest.raises(KeyError):
         db.add_user_report(user_id, job_id, "Garbage")
 
 def test_add_user_report_good(temp_user):
-    user_id = "507f1f77bcf86cd799439011"
-    job_id = "507f191e810c19729de860ea"
+    user_id = ObjectId("507f1f77bcf86cd799439011")
+    job_id = ObjectId("507f191e810c19729de860ea")
 
     assert db.add_user_report(user_id, job_id, "Garbage")
