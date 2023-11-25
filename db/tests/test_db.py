61c389038bc5e3956b68f9ba5851ab905274cb2d
TanzHasan <tanzirhasanhasan@gmail.com>
2023-11-25 17:04:54 -0500

    added test for get users added fixture for testing, dropped db for consistent testing

--- db/tests/test_db.py
+++ db/tests/test_db.py
@@ -5,10 +5,12 @@ import db.db_connect as dbc
 
 TEST_DB = dbc.DB_NAME
 
+user_id = ObjectId("507f1f77bcf86cd799439011")
+job_id = ObjectId("507f191e810c19729de860ea")
+dbc.client.drop_database(TEST_DB)
+
 @pytest.fixture(scope='function')
 def temp_user():
-    user_id = ObjectId("507f1f77bcf86cd799439011")
-    job_id = ObjectId("507f191e810c19729de860ea")
     dbc.client[TEST_DB]["users"].insert_one({"_id": user_id, "username": "GeometryDash"})
     dbc.client[TEST_DB]["jobs"].insert_one({"_id": job_id, "description": "Janitor"})
     # yield to our test function
@@ -16,15 +18,21 @@ def temp_user():
     dbc.client[TEST_DB]["users"].delete_one({"_id": user_id})
     dbc.client[TEST_DB]["jobs"].delete_one({"_id": job_id})
 
-def test_add_user_report_bad():
-    user_id = ObjectId("507f1f77bcf86cd799439011")
-    job_id = ObjectId("507f191e810c19729de860ea")
+@pytest.fixture(scope='function')
+def temp_posting(temp_user):
+    res = db.add_user_report(user_id, job_id, "Garbage")
+    yield
+    dbc.del_one("user_reports", {"_id": res.inserted_id})
 
+def test_add_user_report_bad():
     with pytest.raises(KeyError):
         db.add_user_report(user_id, job_id, "Garbage")
 
 def test_add_user_report_good(temp_user):
-    user_id = ObjectId("507f1f77bcf86cd799439011")
-    job_id = ObjectId("507f191e810c19729de860ea")
+    res = db.add_user_report(user_id, job_id, "Garbage")
+    assert res
+    dbc.del_one("user_reports", {"_id": res.inserted_id})
 
-    assert db.add_user_report(user_id, job_id, "Garbage")
+def test_get_user_report(temp_user, temp_posting):
+    b = db.get_user_reports()
+    assert len(b) == 1
