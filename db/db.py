99462cc8295ff8f264e7cee1b218cb4fca371de5
koswjjnd <sj3192@nyu.edu>
2023-11-26 23:51:04 -0500

    revised

--- db/db.py
+++ db/db.py
@@ -1,4 +1,5 @@
 import db.db_connect as dbc
+import datetime
 """
 This file will manage interactions with our data store.
 At first, it will just contain stubs that return fake data.
@@ -13,13 +14,15 @@ job_data = {
             "data": {
                 "keywords": ["internship"]
             },
-            "userid": 1
+            "userid": 1,
+            "date": datetime.datetime(2020,5,17)
         },
         2: {
             "data": {
                "keywords": ["Remote"]
             },
-            "userid": 2
+            "userid": 2,
+            "date": datetime.datetime(2020,5,18)
         }
     }
 
@@ -109,9 +112,10 @@ def delete_job_past_date(admin_id, past_date):
     if not dbc.exists_by_id(admin_id, "admins"):
         raise KeyError(f"No admin {admin_id}")
     for i in job_data:
-        if job_data[i][date]<past_date:
+        if job_data[i][date] < past_date:
             dbc.del_one("jobs", i)
     return True
+
 
 def get_most_recent_job(user_id, numbers):
     # connect to mongodb to get the numbers of jobs based
