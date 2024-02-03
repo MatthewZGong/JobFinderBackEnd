d350f0b26b089632039c45ed7ec29b421b0b1553
MatthewZGong <matthewzgong@gmail.com>
2024-02-03 17:24:31 -0500

    testing redeploy 2

--- server/endpoints.py
+++ server/endpoints.py
@@ -39,7 +39,7 @@ ADMIN_DELETE_PAST_DATE = "admin_delete_past_date"
 DELETE_USER_REPORT = "delete_user_report"
 GET_JOBS_BASED_ON_PREFERENCE = "get_jobs_by_preferences"
 HELLO_EP = "/hello-world"
-HELLO_RESP = "hello"
+HELLO_RESP = "HELLO WORLD"
 
 
 @api.route(HELLO_EP)
