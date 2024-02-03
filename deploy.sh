baeaec33613188e1997b144469f415216d2e2cab
MatthewZGong <matthewzgong@gmail.com>
2024-02-03 17:41:17 -0500

    added api token secret

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ echo "PA password = $DEMO_PA_PWD"
 echo "SSHing to PythonAnywhere."
 
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN ./rebuild.sh
+    cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN API_TOKEN=$PA_API_TOKEN ./rebuild.sh
 EOF
