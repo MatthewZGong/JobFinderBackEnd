01c6cf0592b85f38ead112c66976adc00a45af37
MatthewZGong <matthewzgong@gmail.com>
2024-05-05 12:47:41 -0400

    deploy open ai token

--- deploy.sh
+++ deploy.sh
@@ -21,5 +21,5 @@ echo "PA password = $DEMO_PA_PWD"
 echo "SSHing to PythonAnywhere."
 
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
-    cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN API_TOKEN=$PA_API_TOKEN ./rebuild.sh
+    cd ~/$PROJ_DIR; PA_USER=$PA_USER PROJ_DIR=~/$PROJ_DIR VENV=$VENV PA_DOMAIN=$PA_DOMAIN API_TOKEN=$PA_API_TOKEN OPENAI_API_KEY=$PA_OPEN_AI_TOKEN ./rebuild.sh
 EOF
